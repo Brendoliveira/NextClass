7003a9b3b0e8fb31d5d79917cab017323f9a15a6
Brendo Queiroz <165160866+Brendoliveira@users.noreply.github.com>
2026-05-15 15:52:43 -0300

    Correção no login/cadastro, atualização navbar

--- Javascript/navbar.js
+++ Javascript/navbar.js
@@ -4,3 +4,25 @@
   menuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
   });
+
+  const navBar = document.querySelector(".navbar");
+
+let prevScrollPos = window.scrollY;
+
+window.addEventListener("scroll", function () {
+
+    let currScrollPos = window.scrollY;
+
+    if (currScrollPos > prevScrollPos) {
+
+        // DESCENDO
+        navBar.style.transform = "translateY(-105%)";
+
+    } else {
+
+        // SUBINDO
+        navBar.style.transform = "translateY(0%)";
+    }
+
+    prevScrollPos = currScrollPos;
+});
